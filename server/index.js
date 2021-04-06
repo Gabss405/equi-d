a7ffa744b2b63c1f8ee271c1ea32a8379361b729
Gabor <ghalasz1988@gmail.com>
2021-04-06 17:23:23 +0100

    feat(heroku stuff): heroku

--- server/index.js
+++ server/index.js
@@ -1,17 +1,15 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
 
-const router = require("./router");
+const router = require('./router');
 
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
-const PORT = 3015;
+const PORT = process.env.PORT || 3015;
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, () =>
-  console.log(`🚀 Server running at http://localhost:${PORT} 🚀 `)
-);
+app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT} 🚀 `));
