b79e213970cb8e424c606bfec4a62669595467a3
Gabor <ghalasz1988@gmail.com>
2021-04-06 14:18:18 +0100

    feat(created proxy in package.json client side): proxy in client

--- server/index.js
+++ server/index.js
@@ -1,17 +1,36 @@
-const express = require("express");
-require("dotenv").config();
+const express = require('express');
+require('dotenv').config();
 
-const router = require("./router");
+const router = require('./router');
 
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
-const PORT = 3015;
+const PORT = process.env.PORT || 3015;
 
-app.use(cors());
+
+const whitelist = ['http://localhost:3000'​, 'http://localhost:3015'​]
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, () =>
-  console.log(`🚀 Server running at http://localhost:${PORT} 🚀 `)
-);
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT} 🚀 `));
